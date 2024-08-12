a394582909bf24ab076b3c687340b35ed58dfd56
vaibhav soni <136541227+Vaibhavsoni007@users.noreply.github.com>
2024-08-12 10:38:46 +0530

    Delete problem06.py

--- problem06.py
+++
@@ -1,8 +0,0 @@
-# Create a program that calculates and displays the perimeter
-# of a circle, prompting the user for the radius
-
-import math
-radius = float(input("enter the radius of the circle :"))
-
-perimeter = 2*math.pi*radius
-print("the perimeter of the circle is :",perimeter)
