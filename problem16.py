67cf2582e6c94a36fe85ccb2d13030a0c6877687
vaibhav soni <136541227+Vaibhavsoni007@users.noreply.github.com>
2024-08-12 10:40:00 +0530

    Delete problem16.py

--- problem16.py
+++
@@ -1,9 +0,0 @@
-# Make a program that asks for a person's age and displays whether they
-# are of legal age or not
-
-age = int(input("Enter your age here :"))
-
-if(age>=18):
-    print("you are legal.")
-else:
-    print("you are illegal")
