17dc1952eaec9891e24e880879a9d295b22a92f5
vaibhav soni <136541227+Vaibhavsoni007@users.noreply.github.com>
2024-08-12 10:38:56 +0530

    Delete problem15.py

--- problem15.py
+++
@@ -1,9 +0,0 @@
-#  Create a program that prompts the user for the radius of a
-# sphere and calculates and displays its volume.
-
-import math
-radius = float(input("Enter the radius of sphere :"))
-
-volume = (4/3)*math.pi* radius**3
-
-print("the volume of the sphere is :",volume)
