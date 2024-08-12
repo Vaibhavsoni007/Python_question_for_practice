739b2e13aafc0d69bdc5d7348924e7426b0283bf
vaibhav soni <136541227+Vaibhavsoni007@users.noreply.github.com>
2024-08-12 10:37:02 +0530

    Delete problem02.py

--- problem02.py
+++
@@ -1,9 +0,0 @@
-# Write a program that calculates the arithmetic mean of two
-# numbers
-
-num1 = float(input("enter a number"))
-num2 = float(input("enter another number"))
-
-mean = (num1+num2)/2
-
-print("the mean of the numbers is :",mean)
